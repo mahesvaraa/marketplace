89e0dc92cd0c96c978ed44fb4e96c47d66eb7022
Данила Шкирдов <dashkirdov@basistech.ru>
2025-02-23 21:04:28 +0400

    Fix

--- market_seller/main.py
+++ market_seller/main.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import asyncio
 import os
 import time
